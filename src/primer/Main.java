683d7d950a08129b5f601b3a3e5a416e3e7410b6
Maks Burenko <Cherniymaks@yandex.ru>
2022-08-16 15:16:21 +0300

    Initial commit

---
+++ src/primer/Main.java
@@ -0,0 +1,19 @@
+package primer;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static IntStream pseudoRandomStream(int seed) {
+        IntStream.of(seed)
+                .map(x -> {
+                    while ()
+                })
+                .forEachOrdered();
+        return null;
+    }
+}
