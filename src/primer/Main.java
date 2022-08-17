f4d173383d6b768c738e27d5bfb59d0477cff21a
Maks Burenko <Cherniymaks@yandex.ru>
2022-08-17 08:13:31 +0300

    123

--- src/primer/Main.java
+++ src/primer/Main.java
@@ -11,9 +11,28 @@ public class Main {
     public static IntStream pseudoRandomStream(int seed) {
         IntStream.of(seed)
                 .map(x -> {
-                    while ()
+                    for (int i = 0; i > -1; i++) {
+                        if (i != 0) {
+                            x = x * x;
+                        }
+                    }
+                    return x;
                 })
-                .forEachOrdered();
-        return null;
+        return IntStream.forEachOrdered(x ->mid(x));
+    }
+
+    public static int mid(int x) {
+        String numbers = Integer.toString(x);
+        String finalNumber = "";
+
+        char[] strToArray = numbers.toCharArray();
+
+        for (int i = 0; i < strToArray.length; i++) {
+            if (i > 1 && i < 5) {
+                finalNumber = finalNumber + x;
+            }
+        }
+        int fN = Integer.parseInt(finalNumber);
+        return fN;
     }
 }
