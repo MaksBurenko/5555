93f72406ed59c1b0505433855d073fe74616989f
Maks Burenko <Cherniymaks@yandex.ru>
2022-08-22 12:41:25 +0300

    123

--- src/primer/Main.java
+++ src/primer/Main.java
@@ -1,36 +1,16 @@
 package primer;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(pseudoRandomStream(13));
     }
 
     public static IntStream pseudoRandomStream(int seed) {
-        return IntStream.of(seed)
-                .map(x -> {
-                    for (int i = 0; i > -1; i++) {
-                        if (i != 0) {
-                            x = x * x;
-                        }
-                    }
-                    return x;
-                })
-                .forEachOrdered(x ->mid(x));
-    }
-
-    public static int mid(int x) {
-        String numbers = Integer.toString(x);
-        String finalNumber = "";
-
-        char[] strToArray = numbers.toCharArray();
-
-        for (int i = 0; i < strToArray.length; i++) {
-            if (i > 1 && i < 5) {
-                finalNumber = finalNumber + x;
-            }
-        }
-        int fN = Integer.parseInt(finalNumber);
-        return fN;
+        return IntStream.iterate(seed, x -> (x*x/10) % 1000);
     }
 }
